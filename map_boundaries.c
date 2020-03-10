e6cb6bf8c0851f5f98d1eb17ba183fe91429acbb
Victor <victor.dedios@outlook.com>
2020-03-10 13:44:13 +0100

    added output colors 🎨

--- map_boundaries.c
+++ map_boundaries.c
@@ -14,9 +14,13 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 #include <stdio.h>
 #include "get_next_line.h"
+
+#define GRN "\e[0;32m"
+#define YLL "\e[0;33m"
+#define RED "\e[0;31m"
+#define RST "\e[0m"
 
 /*
  ** Flood fill recursive algorithm
@@ -82,14 +86,33 @@ int		ft_find_player_pos(int *i, int *j, char **map)
 	return (0);
 }
 
-void	ft_printmap(char **map)
+void	ft_printmap(char **map, int ok)
 {
 	int i;
+	int j;
 
 	i = 0;
+	j = 0;
 	while (map[i]) 
 	{
-		printf("%s\n", map[i]);
+		while (map[i][j])
+		{
+			if (map[i][j] == 'N' || map[i][j] == 'S' ||
+					map[i][j] == 'E' || map[i][j] == 'W')
+				printf(YLL "%c" RST, map[i][j]);
+			else if (map[i][j] == '5')
+			{
+				if (ok)
+					printf(GRN "%c" RST, map[i][j]);
+				else if (!ok)
+					printf(RED "%c" RST, map[i][j]);
+			}
+			else
+				printf("%c", map[i][j]);
+			j++;
+		}
+		printf("\n");
+		j = 0;
 		i++;
 	}
 }
@@ -101,12 +124,14 @@ int		ft_flood_fill(char **map, int i, int j, int lines)
 		if((map[i - 1][j] == ' ' && map[i + 1][j] == ' ')
 				|| (map[i][j + 1] == ' ' && map[i][j - 1] == ' '))
 		{
+			ft_printmap(map, 0);
 			perror("Wrong map :(	");
 			exit(EXIT_FAILURE);
 		}
 	}
 	if ((i < 0) || !map[i][j]  || (j < 0) ||(i == lines))
 	{
+		ft_printmap(map, 0);
 		perror("Wrong map :(	");
 		exit(EXIT_FAILURE);
 	}
@@ -137,10 +162,12 @@ int main(int argc, char **argv)
 	lines = ft_count_lines(argv);
 	map = ft_save_map(lines, argv);
 	ft_find_player_pos(&i, &j, map);
-	ft_printmap(map);
+	printf("----------ORIGINAL MAP---------\n");
+	ft_printmap(map, 1);
 	map[i][j] = '0';
+	printf("----------CALCULATED MAP---------\n");
 	ft_flood_fill(map, i, j, lines);
-	ft_printmap(map);
+	ft_printmap(map, 1);
 	printf("Right map!\n");
 	return (0);
 }
